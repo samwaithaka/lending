a8e82475e23fdcd89b3912ec7cc2afcf4f763005
Samuel Waithaka <samwaithaka@gmail.com>
2025-03-27 07:41:40 +0300

    Retry config

---
+++ src/main/java/com/lending/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.lending.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class AppConfig {}
