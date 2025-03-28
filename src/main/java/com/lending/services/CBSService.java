6b3663e8e0cf85c61b12057e522766f83e2b2cb0
samwaithaka <samwaithaka@gmail.com>
2025-03-28 15:54:22 +0300

    Update CBSService.java

--- src/main/java/com/lending/services/CBSService.java
+++ src/main/java/com/lending/services/CBSService.java
@@ -25,21 +25,24 @@ public class CBSService {
 	private HttpClient httpClient;
 	
 	public Customer getKYC(String customerNumber) {
-		String message = SoapMessage.getKYCRequestMessage(customerNumber);
-		Customer customer = null;
-		try {
-			XmlMapper xmlMapper = new XmlMapper();
-			ObjectMapper jsonMapper = new ObjectMapper();
-			jsonMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-			//String xmlRes = httpClient.sendPostRequest(KYCEndpoint, message, Format.SOAP, null);
-			String xmlRes = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"                   xmlns:tns=\"http://credable.io/cbs/customer\">    <soapenv:Header/>    <soapenv:Body>       <tns:CustomerResponse>          <customer>             <id>20</id>             <accountNumber>9876543210</accountNumber>             <createdAt>2025-03-24T10:15:30Z</createdAt>             <createdDate>2025-03-24T10:15:30Z</createdDate>             <dob>2025-03-24</dob>             <email>samuel@gmail.com</email>             <firstName>John</firstName>             <middleName>Smith</middleName>             <lastName>Doe</lastName>             <gender>MALE</gender>             <idNumber>203030303</idNumber>             <idType>NATIONAL_ID</idType>             <mobile>0703393928</mobile>             <monthlyIncome>4</monthlyIncome>             <status>2500.00</status>             <updatedAt>2025-03-24T10:15:30Z</updatedAt>           </customer>       </tns:CustomerResponse>    </soapenv:Body> </soapenv:Envelope>";
-			JsonNode jsonFromSoap = xmlMapper.readTree(xmlRes.getBytes());
-			JsonNode responseObject = jsonFromSoap.get("Body").get("CustomerResponse").get("customer");
-			customer = jsonMapper.readValue(responseObject.toString(), new TypeReference<Customer>() {});
-		} catch (Exception e) {
-			e.printStackTrace();
+		Customer customer = findExistingCustomer(customerNumber);
+		if(customer != null) {
+			return customer;
+		} else {
+			String message = SoapMessage.getKYCRequestMessage(customerNumber);
+			try {
+				XmlMapper xmlMapper = new XmlMapper();
+				String xmlRes = httpClient.sendPostRequest(KYCEndpoint, message, Format.SOAP, null);
+				JsonNode jsonFromSoap = xmlMapper.readTree(xmlRes.getBytes());
+				JsonNode responseObject = jsonFromSoap.get("Body").get("transactionData");
+				customer = new ObjectMapper().readValue(responseObject.toString(), Customer.class);
+				customerRepo.save(customer);
+				return customer;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return new Customer();
+			}
 		}
-		return customer;
 	}
 
 	public Customer findExistingCustomer(String customerNumber) {
